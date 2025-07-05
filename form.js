5f00d40454f3a49f7aeae7178b450f555bc5a681
luke fischbeck <lfischbeck@gmail.com>
2025-07-05 19:59:01 -0400

    Update form.js
    add absolute URL for JSON

--- form.js
+++ form.js
@@ -6,7 +6,7 @@
 
 const main = document.getElementById("main");
 const index = document.getElementById('index');
-const requestURL = "/real-time-communication/RTC.json";
+const requestURL = "https://publicityreform.github.io/real-time-communication/RTC.json";
 
 let request = new XMLHttpRequest();
 
